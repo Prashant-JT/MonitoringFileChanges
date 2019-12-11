200f94d7dfc42a621dfecf66d1612f2bb0bca41b
eduardomaldonadofernandez21 <55493415+eduardomaldonadofernandez21@users.noreply.github.com>
2019-12-11 12:43:45 +0000

    Add files via upload

---
+++ snapshot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+bin="/bin"
+usrBin="/usr/bin"
+sbin="/sbin"
+usrSbin="/usr/sbin"
+
+declare -A map
+
+functionFind()
+{
+	for file in $(find $bin)
+	do
+		if [[ ! -d $file ]]; then
+			map[$(md5sum $file)]="$(stat --printf="%a" $file)"
+		else
+			map[$file]="$(stat --printf="%a" $file)"
+		fi
+		echo "$map[$(md5sum $file)]" "${map[$(md5sum $file)]}"	
+
+	done
+}
+
+die() {
+    echo $1 >&2
+    exit 1
+}
+
+
+
+functionFind
+
